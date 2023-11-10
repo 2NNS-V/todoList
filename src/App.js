164eb079bfbcc1282d2889ab9499c7c15d295110
2NNS-V <msy3824@naver.com>
2023-11-10 15:24:11 +0900

    container 및 todoBlock 생성

--- src/App.js
+++ src/App.js
@@ -1,12 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {Component} from "react";
+import "./App.css";
 
-function App() {
-  return (
-    <div class = "App">
-      hello React!
-    </div>
-  );
+export default class App extends Component {
+  redner() {
+    return (
+      <div className = "container">
+        <div className = "todoBlock">
+          <div className = "title">
+            <h1>할 일 목록</h1>
+          </div>
+
+        </div>
+      </div>
+    )
+  }
 }
-
-export default App;
